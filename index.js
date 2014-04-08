1c8b7d902a9a8f851cf41bc184343deceac8df51
Steven Zeiler <zeiler.steven@gmail.com>
2014-04-07 17:19:01 -0700

    [FEATURE] Refactor computation of private and public generators to functions.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Base58 = Base58Utils = require('./lib/base58')
+"use strict";
+
+var Base58 = require('./lib/base58')
 var Ripple = require('ripple-lib');
 var sjcl = Ripple.sjcl;
 
@@ -19,7 +21,7 @@ var RippleWallet = (function () {
   }
 
   return function (seed) {
-    this.seed = Base58Utils.decode_base_check(33, seed);
+    this.seed = Base58.decode_base_check(33, seed);
 
     if (!this.seed) {
       throw "Invalid seed."
@@ -28,22 +30,37 @@ var RippleWallet = (function () {
     this.getAddress = function (seq) {
       seq = seq || 0;
 
-      var private_gen, public_gen, i = 0;
-      do {
-        // Compute the hash of the 128-bit seed and the sequence number
-        private_gen = sjcl.bn.fromBits(firstHalfOfSHA512(append_int(this.seed, i)));
-        i++;
-        // If the hash is equal to or greater than the SECp256k1 order, increment sequence and try agin
-      } while (!sjcl.ecc.curves.c256.r.greaterEquals(private_gen));
+      function computePrivateGenerator(seed) {
+        let i = 0, private_gen;
+        
+        do {
+          // Compute the hash of the 128-bit seed and the sequence number
+          private_gen = sjcl.bn.fromBits(firstHalfOfSHA512(append_int(seed, i)));
+          i++;
+          // If the hash is equal to or greater than the SECp256k1 order, increment sequence and try agin
+        } while (!sjcl.ecc.curves.c256.r.greaterEquals(private_gen));
 
-      // Compute the public generator using from the private generator on the elliptic curve
-      public_gen = sjcl.ecc.curves.c256.G.mult(private_gen);
+        return {
+          generator: private_gen,
+          sequence: i
+        }
+      }
+
+      function computePublicGenerator(privateGenerator) {
+        return sjcl.ecc.curves.c256.G.mult(privateGenerator);
+      }
+
+      var computed = computePrivateGenerator(seed);
+
+      var privateGenerator = computed.generator;
+      var sequence = computed.sequence;
+      var publicGenerator = computePublicGenerator(privateGenerator);
 
       var sec;
-      i = 0;
+      var i = 0;
       do {
         // Compute the hash of the public generator with sub-sequence number
-        sec = sjcl.bn.fromBits(firstHalfOfSHA512(append_int(append_int(public_gen.toBytesCompressed(), seq), i)));
+        sec = sjcl.bn.fromBits(firstHalfOfSHA512(append_int(append_int(publicGenerator.toBytesCompressed(), sequence), i)));
         i++;
         // If the hash is equal to or greater than the SECp256k1 order, increment the sequence and retry
       } while (!sjcl.ecc.curves.c256.r.greaterEquals(sec));
@@ -51,26 +68,46 @@ var RippleWallet = (function () {
       // Treating this hash as a private key, compute the corresponding public key as an EC point. 
       // Add this EC point to the public generator EC point. 
       // The resulting point is the public key for this account. 
-      var pubKey = sjcl.ecc.curves.c256.G.mult(sec).toJac().add(public_gen).toAffine();
+      var pubKey = sjcl.ecc.curves.c256.G.mult(sec).toJac().add(publicGenerator).toAffine();
 
       // Finally encode the EC public key as a ripple address using SHA256 and then RIPEMD160
-      return Base58Utils.encode_base_check(0, sjcl.codec.bytes.fromBits(SHA256_RIPEMD160(sjcl.codec.bytes.toBits(pubKey.toBytesCompressed()))));
+      return Base58.encode_base_check(0, sjcl.codec.bytes.fromBits(SHA256_RIPEMD160(sjcl.codec.bytes.toBits(pubKey.toBytesCompressed()))));
     };
   };
 })();
 
-RippleWallet.generate = function() {
-  for (var i = 0; i < 8; i++) {
+
+RippleWallet._generateMasterSeed = function() {
+  for (let i = 0; i < 8; i++) {
     sjcl.random.addEntropy(Math.random(), 32, "Math.random()");
   }
 
-  // Generate a 128-bit master key that can be used to make any number of private / public key pairs and accounts
-  var masterkey = Base58.encode_base_check(33, sjcl.codec.bytes.fromBits(sjcl.random.randomWords(4)))
-  address = new RippleWallet(masterkey)
+  var seed = Base58.encode_base_check(33, sjcl.codec.bytes.fromBits(sjcl.random.randomWords(4)));
+  return seed;
+}
+
+RippleWallet.generate = function() {
+  var masterSeed = this._generateMasterSeed();
+  let address = new RippleWallet(masterSeed);
 
   return {
     address: address.getAddress(),
-    secret: masterkey 
+    secret: masterSeed 
+  }
+}
+
+RippleWallet._computePrivateGenerator = function(seed) {
+  let i = 0;
+  do {
+    // Compute the hash of the 128-bit seed and the sequence number
+    let private_gen = sjcl.bn.fromBits(firstHalfOfSHA512(append_int(seed, i)));
+    i++;
+    // If the hash is equal to or greater than the SECp256k1 order, increment sequence and try agin
+  } while (!sjcl.ecc.curves.c256.r.greaterEquals(private_gen));
+
+  return {
+    generator: private_gen,
+    sequence: i
   }
 }
 
